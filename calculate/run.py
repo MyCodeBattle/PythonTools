5cd36ad99811efbc83b45d0a10406d79584ee2b7
catsith <mycodebattle@outlook.com>
2022-03-20 09:38:52 +0800

    貌似最终版

--- calculate/run.py
+++ calculate/run.py
@@ -1,9 +1,6 @@
 import pandas as pd
-df = pd.read_excel('/Users/zhuolx/Downloads/运费.xlsx', index_col='地区', sheet_name=None)
-print(df['旭昇'])
+dfs = pd.read_excel('/Users/zhuolx/Downloads/电商计算/模板/运费.xlsx', index_col='地区', sheet_name=None)
+df = dfs['长沙仓']
+print(df.columns)
+print(df['wei<=0.5'].values[0])
 wei = 0.6
-for c in df.columns:
-    if c == '地区':
-        continue
-    if eval(c):
-        print(df.loc['浙江', c])
